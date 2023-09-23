10691e24b3fbc25af2b1373b921bb1b64e0b5913
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2023-09-23 14:15:28 -0400

    refactor and add pokemon to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
+let pokeContainer = document.getElementById('pokemon');
+
 //TODO
 // Loop over all Types in API
-
 let pokeTypeUrl = `https://pokeapi.co/api/v2/type/1`
 
-
-function pokeTypeRetrieve() {
+function getPokeType() {
   fetch(pokeTypeUrl)
   .then(function (response) {
     return response.json();
@@ -12,15 +12,24 @@ function pokeTypeRetrieve() {
   .then(function (data){
     console.log(data.pokemon);
     let pokeNamesType = data.pokemon
+
     for (let i = 0; i < pokeNamesType.length; i++) {
       console.log(i);
-      let pokeNames = pokeNamesType[i].pokemon.name;
-      let pokeUrls = pokeNamesType[i].pokemon.url;
-      console.log(pokeNames);
-      console.log(pokeUrls);
+      console.log(pokeNamesType[i].pokemon.name);
+      console.log(pokeNamesType[i].pokemon.url);
+
+      let pokeNames = document.createElement('h3');
+      let pokeUrl = document.createElement('p');
+
+      pokeNames.textContent = pokeNamesType[i].pokemon.name
+      pokeUrl.textContent = pokeNamesType[i].pokemon.url
+
+      pokeNames.setAttribute('class', "has-text-centered")
+
+      pokeContainer.append(pokeNames);
+      pokeContainer.append(pokeUrl);
 
     }
   }) 
 }
-
-pokeTypeRetrieve();
+getPokeType();
