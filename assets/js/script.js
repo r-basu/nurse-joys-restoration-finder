5c0d9bc9bbc359aa45e2cd8174788a255b13b7f2
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2023-09-28 20:45:56 -0400

    poke selection update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,17 @@
 let pokeContainer = document.getElementById('pokemon');
-let pokeBerryUrl = `https://pokeapi.co/api/v2/berry`
-
-var pokemonType = localStorage.pokemonType;
+let pokemonType = localStorage.pokemonType;
 
 function getPokeType() {
   let pokeTypeUrl = `https://pokeapi.co/api/v2/type/${pokemonType}`
   fetch(pokeTypeUrl)
     .then(response => response.json())
     .then(function (data) {
-      console.log(data.pokemon);
-      let pokeNamesType = data.pokemon
-      for (let i = 0; i < pokeNamesType.length; i++) {
+      for (let i = 0; i < data.pokemon.length; i++) {
         let pokeContainer = document.getElementById('dark_select');
-        console.log(i);
-        console.log(pokeNamesType[i].pokemon.name);
-
         let pokeNames = document.createElement('option');
 
-        pokeNames.textContent = pokeNamesType[i].pokemon.name;
-        pokeNames.setAttribute("value", pokeNamesType[i].pokemon.name);
+        pokeNames.textContent = data.pokemon[i].pokemon.name;
+        pokeNames.setAttribute("value", data.pokemon[i].pokemon.name);
 
         pokeContainer.append(pokeNames);
       }
@@ -29,7 +22,13 @@ getPokeType();
 function getPokemon() {
   let pokeDropdown = document.getElementById("dark_select");
   let pokeSelectDrop = pokeDropdown.value;
-  let pokeUrl = `https://pokeapi.co/api/v2/type/${pokeSelectDrop}`
-  console.log(pokeUrl)
+  let pokeUrl = `https://pokeapi.co/api/v2/pokemon/${pokeSelectDrop}`
+  fetch(pokeUrl)
+    .then(response => response.json())
+    .then(function (data) {
+      let pokeContainer = document.getElementById("pokeSelect");
+      let pokeData = document.createElement('p')
+      console.log(data.name);
+    })
 }
 getPokemon();
