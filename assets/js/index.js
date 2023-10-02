73798e990306ca6e830fbf431a4a4dd9801e37c8
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2023-10-02 19:04:03 -0400

    Centered items

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,7 @@
-var pokemonTypeEl = document.querySelector(`.tAlign`);
-var randomClick = document.querySelector(`.randomClick`);
-var pokemonType;
+let pokemonTypeEl = document.querySelector(`.tAlign`);
+let pokemonType;
 
-randomClick.addEventListener("click", function (event) {
-  window.location.assign(
-    "./result.html"
-  );
-})
+
 pokemonTypeEl.addEventListener("click", function (event) {
   pokemonType = event.target.id;
   localStorage.pokemonType = pokemonType; // local storage where pokemon type is being stored.
