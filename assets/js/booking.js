54e2395c6e8323e44a853bd1baa1172f99102480
Vinit Patel <vinitpatel476@gmail.com>
2023-10-02 20:08:25 -0400

    updated Bookings page

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -1,2 +1,11 @@
 var timeSelected = document.querySelector(`#timeSelected`);
-console.log(timeSelected);
+var dateSelected = document.querySelector(`#dateSelected`);
+var submitBtn = document.querySelector(`#submitBtn`);
+
+
+submitBtn.addEventListener("click", function(event){
+    console.log(timeSelected.value);
+    console.log(dateSelected.value);
+    localStorage.timeSelected = timeSelected.value;
+    localStorage.dateSelected = dateSelected.value;
+});
